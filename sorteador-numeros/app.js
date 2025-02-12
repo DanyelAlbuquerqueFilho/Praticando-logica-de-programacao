a9f2452feb0fb4b5b82903030555043ca99b6106
DanyelAlbuquerqueFilho <danyelalbuquerquefilho@gmail.com>
2025-02-12 18:31:43 -0300

    Função de sortear funcionando

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -6,18 +6,25 @@ function sortear(){
     let sorteados = [];
     let numero;
     for(let i = 0; i < quantidade; i++) {
-        numero = obterNumeroAleatorio(de, ate);
-        sorteados.push(numero);
-    }
+        if(quantidade > (ate - de + 1)) {
+            alert("A quantidade de números a serem sorteados não pode ser maior que o intervalo definido.");
+            return;
+        }
 
-    alert(sorteados);
+        numero = obterNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)){
+            numero = obterNumeroAleatorio(de,ate);
+        }
+
+        sorteados.push(numero);
+    } 
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
 
 }
 
 function obterNumeroAleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function exibirNumerosSorteados(){
-    
 }
