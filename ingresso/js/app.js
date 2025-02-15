b146644824eae9f201a507c20e649bd6762d0fec
DanyelAlbuquerqueFilho <danyelalbuquerquefilho@gmail.com>
2025-02-14 21:24:40 -0300

    Ingresso + desafio feitos

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -2,6 +2,11 @@ function comprar() {
     let tipoDeIngresso = document.getElementById('tipo-ingresso').value;;
     let qtd = parseInt(document.getElementById('qtd').value);
 
+    if(qtd < 0) {
+        alert('Digite um número positivo ! ');
+        return;
+    }
+
     if(tipoDeIngresso == 'pista'){
         comprarPista(qtd);
     } else if(tipoDeIngresso == 'superior') {
@@ -45,3 +50,63 @@ function comprarInferior(qtd){
         alert('Compra realizada com sucesso! ');
     }
 }
+
+function stringInt(string) {
+    return parseInt(string);
+}
+
+function adicao(a,b) {
+    return a + b;
+}
+function subtracao(a,b) {
+    return a - b;
+}
+function multiplicacao (a,b) {
+    return a * b;
+}
+function divisao(a,b) {
+    if(b !== 0) {
+        return a/b 
+    } else {
+        return 'Erro';
+    }
+}
+
+function calculadora(operacao, a,b) {
+    switch(operacao) {
+        case 'soma': 
+            return adicao(a,b);
+        case 'subtracao':
+            return subtracao(a,b);
+        case 'multiplicacao':
+            return multiplicacao(a,b);
+        case 'divisao':
+            return divisao(a,b);
+    }
+}
+
+function imparPar(numero) {
+    if((numero%2) == 0) {
+        console.log('Numero é par');
+    }else {
+        console.log('Numero é impar');
+    }
+}
+
+function conversorTemperatura(temperatura, escala){
+    if( escala == 'C') {
+        return celsius(temperatura);
+    } else if (escala == 'F'){
+        return fr(temperatura)
+    }
+}
+
+function celsius(fr){
+    return (fr - 32)* 5/9
+}
+
+function fr(celsius){
+    return (celsius*1.8) + 32
+}
+
+console.log(conversorTemperatura('20', 'F'))
